1ac7b256540a858cdafb3b38910a8bb941f03861
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2020-09-05 01:33:35 -0400

    Can now successfully upload an image to aws s3 bucket

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://jason:carcamo11@localhost/serenta-api-test",
     JWT_SECRET: process.env.SECRET || "mweifhbwehfewijkfwe",
     NODE_ENV: process.env.NODE_ENV || "development",
-    AccessKeyId: process.env.AccessKeyId,
-    SecretAccessKey: process.env.SecretAccessKey,
-    BucketName: process.env.BucketName
+    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+    BUCKET_NAME: process.env.BUCKET_NAME
 };
--- src/routes/LivingSpaceImagesRouter/LivingSpaceImagesRouter.js
+++ src/routes/LivingSpaceImagesRouter/LivingSpaceImagesRouter.js
@@ -3,60 +3,50 @@ const LivingSpaceImagesRouter = express.Router();
 const LivingSpaceImageService = require("../../services/LivingSpaceImageServices/LivingSpaceImageServices");
 const {requireAuth} = require("../../middleware/jwtAuth");
 const AWS = require("aws-sdk");
-const {BucketName, AccessKeyId, SecretAccessKey} = require("../../../config");
+const {BUCKET_NAME, ACCESS_KEY_ID, SECRET_ACCESS_KEY} = require("../../../config");
 const fs = require("fs");
 const multer = require("multer");
-const upload = multer({dest:"upload/"});
+
+var storage = multer.memoryStorage();
+var upload = multer({ dest: "upload/" });
 
 LivingSpaceImagesRouter
     .route("/living-space-images")
     .get((req, res)=>{
 
     })
-    .post(requireAuth, upload.array("images", 12), (req, res)=>{
-        const {
-            images
-        } = req.body;
-
-        const newImages = {
-            images
-        };
-
+    .post(requireAuth, upload.array("images", Infinity), (req, res)=>{
+        const images = req.files;
         const s3 = new AWS.S3({
-            accessKeyId: AccessKeyId,
-            secretAccessKey: SecretAccessKey
+            accessKeyId: ACCESS_KEY_ID,
+            secretAccessKey: SECRET_ACCESS_KEY
         });
         let params;
         let imageKey;
 
-        console.log(newImages);
-        console.log(req.file)
-
-        for( const [key, value] of Object.entries(newImages)){
-            console.log("Value", value[0]);
-            if(!value){
-                return res.status(400).json({
-                    error: `Missing ${key} in body request`
-                });
-            };
-        };
-
-        for(let i = 0; i < newImages.images.length; i++){
-    
-
-            ImageData = fs.readFileSync(newImages.images);
-
-            console.log(ImageData)
+        images.forEach((image, index)=>{
+            console.log(image);
+            const fileContent = fs.readFileSync(image.path);
+            console.log("File content", fileContent);
 
             params = {
-                Bucket: BucketName,
-                Key: newImages.images[i].name,
-                Body: ImageData
+                Bucket: BUCKET_NAME,
+                Key: image.originalname,
+                Body: fileContent
             };
-        };
 
-        return res.status(200).json({
-            success: "Finished!"
+            console.log(params)
+            s3.upload(params, (err, data)=>{
+                if(err){
+                    console.log(err);
+                }
+
+                console.log("Successfully uploaded", data)
+                return res.status(200).json({
+                    success: "Finished!"
+                });
+            })
+
         });
     })
 
